13f7d6393329690987988b623df63c9354891aa3
Jaiendiran <jaiendiran2494@gmail.com>
2019-02-08 15:35:19 +0530

    fac cmt3

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 require("../scss/style.scss");
 require("../index.html");
 import * as $ from "jquery/dist/jquery.min";
+import "@material/mdc-fab/mdc-icon-button";
 
 $(document).ready(function() {
     $(".toggler").on("click", function() {
