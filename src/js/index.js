b54656c6ff1f4b8eb74d6a476bfa34e268877687
Jaiendiran <jaiendiran2494@gmail.com>
2019-02-08 23:53:24 +0530

    fab and icbtn

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
 require("../scss/style.scss");
 require("../index.html");
 import * as $ from "jquery/dist/jquery.min";
+import * as mdc from "material-components-web";
 
 $(document).ready(function() {
     $(".toggler").on("click", function() {
         $(".exitedBtn").toggleClass("mdc-fab--exited");
-    })
-})
+    });
+});
+
+$(document).ready(function() {
+    $(".iconToggle").each(function() {
+        new mdc.iconButton.MDCIconButtonToggle(this);
+    });
+});
