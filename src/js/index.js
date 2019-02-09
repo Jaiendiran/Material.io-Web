1bb1b09c6ee30ba319464746d8c81fd5f5892120
Jaiendiran <jaiendiran2494@gmail.com>
2019-02-09 22:57:47 +0530

    ripple working

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ require("../scss/style.scss");
 require("../index.html");
 import * as $ from "jquery/dist/jquery.min";
 import * as mdc from "material-components-web";
+import { MDCRipple } from "@material/ripple";
 
 $(document).ready(function() {
     $(".toggler").on("click", function() {
@@ -13,4 +14,8 @@ $(document).ready(function() {
     $(".iconToggle").each(function() {
         new mdc.iconButton.MDCIconButtonToggle(this);
     });
+    $(".rippleEffect").each(function() {
+        const iconToggleRipple = new MDCRipple(this);
+        iconToggleRipple.unbounded = true;
+    })
 });
